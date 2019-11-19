777e47636f926681ae72df0c05b2c92644419e1f
r06921037zwh <32741450+r06921037zwh@users.noreply.github.com>
2019-11-19 14:43:41 +0100

    Update Unique_Binary_Search_Trees.cpp

--- Unique_Binary_Search_Trees.cpp
+++ Unique_Binary_Search_Trees.cpp
@@ -16,8 +16,8 @@ public:
         f[0] = 1;
         f[1] = 1;
         for(int i=2; i<=n; i++){
-            for(int j=i-1; j>=0; j--){
-                f[i] += f[j] * f[i-j-1];
+            for(int j=1; j<=i; j++){
+                f[i] += f[j-1] * f[i-j];
             }
         }
         return f[n];
