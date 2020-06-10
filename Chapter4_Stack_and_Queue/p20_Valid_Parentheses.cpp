1e64aedba4e0761883e406153e3b949fdb657c51
r06921037zwh <32741450+r06921037zwh@users.noreply.github.com>
2020-06-11 02:12:42 +0800

    Update p20_Valid_Parentheses.cpp

--- Chapter4_Stack_and_Queue/p20_Valid_Parentheses.cpp
+++ Chapter4_Stack_and_Queue/p20_Valid_Parentheses.cpp
@@ -22,3 +22,31 @@ public:
         return stk.empty();
     }
 };
+
+
+
+// Another Method 
+#include <stack>
+#include <unordered_map>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length() == 0) return true;
+        
+        string left = "([{";
+        string right = ")]}";
+        
+        stack<char> stk;
+        for(auto c:s){
+            if(left.find(c) != string::npos){
+                stk.push(c);    
+            }else{
+                if(stk.empty() || stk.top() != left[right.find(c)]) return false;
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
